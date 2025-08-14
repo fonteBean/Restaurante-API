4b7cdba3180371362c53477b6fe43c5834f335f6
fonteBean <n9ckbean@gmail.com>
2025-08-14 11:28:46 -0300

    Add: CRUD repository functions

---
+++ repositories/restaurantRepository.js
@@ -0,0 +1,62 @@
+const db = require('../db/db');
+
+async function create(plate) {
+  try{
+    const query = await db.('db_restaurante').insert(plate);
+    if(!query){
+      return false;
+    }
+    return true;
+}catch(err){
+    console.log(err);
+    return false;
+  }
+}
+
+async function read(id) {
+  try{
+    const plate = await db.('db_restaurante').where({id:id}).first();
+    if(!plate){
+      return false;
+    }
+    return plate;
+  }catch(err){
+    console.log(err);
+    return false;
+  }
+}
+
+async function readAll(){
+  try{
+    const plates = await db.('db_restaurante').select('*');
+    if(!plates){
+      return false;
+    }
+    return true;
+  }catch(err){
+    console.log(err);
+    return false;
+  }
+}
+
+
+async function del(id){
+  try{
+    const query = await db.('db_restaurante').where({id:id});
+    if(!query){
+      return false;
+    }
+    return true;
+  }catch(err){
+    console.log(err);
+    return false;
+  }
+}
+
+
+module.exports = {
+  create,
+  read,
+  readAll,
+  del
+}
