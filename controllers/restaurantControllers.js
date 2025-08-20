f626f0c502e66224c2ec13f1f12c5751a24362b0
fonteBean <n9ckbean@gmail.com>
2025-08-20 18:47:00 -0300

    Add:ErrorHandler

---
+++ controllers/restaurantControllers.js
@@ -0,0 +1,62 @@
+const restaurantRepository = require("../repositories/restaurantRepository")
+const errorResponse = require('../utils/errorHandler')
+async function read(req,res) {
+    try{
+      const id = req.param.id;
+      const plate = await restaurantRepository.read(id);
+      if(!plate){
+        return errorResponse(res,404,`Not Found`);
+      }
+      return res.status(200).json(plate);
+    }catch(error){
+      return errorResponse(res,500,error);
+    }
+}
+
+
+async function create(req,res) {
+  try {
+    const {nome,descricao,preco,ingredientes} = req.body;
+    if(!nome || !descricao || !preco || !ingredientes){
+        return errorResponse(res,400,"Parâmetros incompletos");
+    }
+    const plate = {
+      nome : nome,
+      descricao : descricao,
+      preco : preco,
+      ingredientes : ingredientes
+    }
+    const query =  await restaurantRepository.create(plate);
+    if(!query){
+      return errorResponse(res,400,"Error ao criar prato");
+    }
+    return res.status(201).json(plate);
+  } catch (error) {
+   return errorResponse(res,500,error);
+  }  
+
+}
+
+
+async function del(req,res) {
+  try{
+    const id = req.id;
+    if(!id){
+      return errorResponse(res,404,"Id não encontrado");
+    }
+    const query = await restaurantRepository.del(id);
+    if(!query){
+      return errorResponse(res,400,"Erro ao deletar item");
+    }
+    return res.status(204);
+  }catch(error){
+    return errorResponse(res,500,error);
+  }
+}
+
+module.exports = {
+  read,
+  readAll,
+  create,
+  del
+}
--- repositories/restaurantRepository.js
+++ repositories/restaurantRepository.js
@@ -2,7 +2,7 @@ const db = require('../db/db');
 
 async function create(plate) {
   try{
-    const query = await db.('db_restaurante').insert(plate);
+    const query = await db('db_restaurante').insert(plate);
     if(!query){
       return false;
     }
@@ -15,7 +15,7 @@ async function create(plate) {
 
 async function read(id) {
   try{
-    const plate = await db.('db_restaurante').where({id:id}).first();
+    const plate = await db('db_restaurante').where({id:id}).first();
     if(!plate){
       return false;
     }
@@ -28,11 +28,11 @@ async function read(id) {
 
 async function readAll(){
   try{
-    const plates = await db.('db_restaurante').select('*');
+    const plates = await db('db_restaurante').select('*');
     if(!plates){
       return false;
     }
-    return true;
+    return plates;
   }catch(err){
     console.log(err);
     return false;
@@ -42,7 +42,7 @@ async function readAll(){
 
 async function del(id){
   try{
-    const query = await db.('db_restaurante').where({id:id});
+    const query = await db('db_restaurante').where({id:id});
     if(!query){
       return false;
     }
