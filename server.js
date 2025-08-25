cd69a5c2ac64e537b541c37be72b8dfc6b90ebf9
fonteBean <n9ckbean@gmail.com>
2025-08-25 17:30:59 -0300

    Add:morgan

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const PORT = 3001;
 const routes = require('./routes/restaurantRoutes')
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(morgan('common'));
 app.use(routes);
 
 
